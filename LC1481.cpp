b8e7a8ed643413d983069602b14f9dbc69fac606
Yashasvi R P <yashasvirp@gmail.com>
2023-04-05 14:40:12 +0530

    Added LC1481

---
+++ LC1481.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        
+        vector <int> sum {nums[0]};
+
+        for(int i = 1; i < nums.size(); i++)
+            sum.push_back(sum[i-1] + nums[i]);
+
+        return sum;
+    }
+};
