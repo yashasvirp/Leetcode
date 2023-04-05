127e1569d98c33e1679857d94f3c65c762bbf8ef
Yashasvi R P <yashasvirp@gmail.com>
2023-04-05 17:15:57 +0530

    Added LC876

---
+++ LC876.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        temp1 = head
+        temp2 = head
+
+        while temp2 and temp2.next:
+            temp1 = temp1.next
+            temp2 = temp2.next.next
+        
+        return temp1
