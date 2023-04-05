f166efdb0c00a80fb9bdc064f15fe7876fe7d34c
Yashasvi R P <yashasvirp@gmail.com>
2023-04-05 21:25:33 +0530

    Contains duplicate

---
+++ LC217.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        
+        for i in d:
+            if d[i] > 1:
+                return True
+
+        return False
