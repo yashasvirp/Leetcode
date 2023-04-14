52e770bcdb2e7eb8b387c4b27ff174eee7eacdba
Yashasvi R P <yashasvirp@gmail.com>
2023-04-14 21:24:54 +0530

    Merge Two Sorted Linked Lists

---
+++ LC21.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        if l1 == None:
+            return l2
+        
+        if l2 == None:
+            return l1
+        
+        head = None
+
+        if l1.val < l2.val:
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+        
+        temp = head
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            
+            temp = temp.next
+        
+        if l1 or l2:
+            temp.next = l1 if l1 else l2
+        
+        
+        return head
