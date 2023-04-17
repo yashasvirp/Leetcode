f5de73f99620ab82c375f7696f8b295efbfc7a67
Yashasvi R P <yashasvirp@gmail.com>
2023-04-17 21:16:38 +0530

    Added another iterative solution for PreOrder Traversal

--- LC144.cpp
+++ LC144.cpp
@@ -94,3 +94,44 @@ public:
         return v;
     }
 };
+
+
+//Another easier Iterative solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+
+        vector<int> v;
+        stack<TreeNode *> st;
+
+        if(root == NULL){
+            return v;
+        }
+
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode *temp = st.top();
+            st.pop();
+
+            if(temp){
+                v.push_back(temp->val);
+                st.push(temp->right);
+                st.push(temp->left);
+            }
+        }
+        
+        return v;
+    }
+};
