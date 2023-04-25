214fe5a2fd3f6722df4d3b7865a4dc00a64c2fe4
Yashasvi R P <yashasvirp@gmail.com>
2023-04-25 12:34:36 +0530

    Can make AP

---
+++ LC1502.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+
+        for i in range(len(arr)-2):
+            if arr[i] - arr[i+1] != arr[i+1] - arr[i+2]:
+                return False
+        
+        return True
