5ca10248d54be036ad0d31610d7f521b9ddc1057
Yashasvi R P <yashasvirp@gmail.com>
2023-05-03 10:39:59 +0530

    Added another approach using maps and string replace

--- LC1309.py
+++ LC1309.py
@@ -17,3 +17,25 @@ class Solution:
 
 
         return res
+
+
+# Another approach
+
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        d = []
+
+        for i in range(1,27):
+            v = chr(96 + i)
+            if 1 <= i < 10:
+                d.append([str(i), v])
+                continue
+            r = f"{i}#"
+            d.append([r, v])
+        
+        res = s
+
+        for k,v in d[::-1]:
+            res = res.replace(k,v)
+        
+        return res
