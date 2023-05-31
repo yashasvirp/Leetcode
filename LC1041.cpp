d1272e363ef79ab429fa4a6e8eba60d77a82e435
Yashasvi R P <yashasvirp@gmail.com>
2023-05-31 12:06:32 +0530

    Robot Bounded In Circle

---
+++ LC1041.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        
+        int x = 0, y = 0, i = 0;
+
+        vector <vector <int>> d = {{0,1},{1,0},{0,-1},{-1,0}};
+
+        for(char &s : instructions)
+            if(s == 'R')
+                i = (i+1)%4;
+            else if(s == 'L')
+                i = (i+3)%4;
+            else
+                x += d[i][0], y += d[i][1];
+        
+        return x == 0 && y == 0 || i > 0;
+    }
+};
