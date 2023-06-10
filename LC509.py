4eed4b75270e86e7b91c48a9692ef63b9c2ae659
Yashasvi R P <yashasvirp@gmail.com>
2023-06-10 12:27:31 +0530

    Fibonacci number

---
+++ LC509.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        
+        a = 0
+        b = 1
+        c = 0
+
+        while n > 1:
+            c = a + b
+            a = b
+            b = c
+            n -= 1
+        
+        return c
