8189d0ce9c8f857a7a9ba09986e2b0af61fd48c2
Yashasvi R P <yashasvirp@gmail.com>
2023-06-10 12:11:39 +0530

    Valid string Palindrome

---
+++ LC125.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i = 0, j = n-1;
+
+        while(i < j){
+            if(!isalnum(s[i]))
+                i++;
+            else if(!isalnum(s[j]))
+                j--;
+            
+            else if(tolower(s[i]) != tolower(s[j]))
+                return false;
+            else
+            {i++; j--;}
+        }
+        return true;
+    }
+};
