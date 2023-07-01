5ec1c4536a55525a399479252b772b2ba29f701f
Yashasvi R P <yashasvirp@gmail.com>
2023-07-01 17:02:34 +0530

    Design Hashset

---
+++ LC705.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+
+private:
+    vector<bool> arr;    
+public:
+    
+    MyHashSet() {
+        arr.resize(1000001, false);
+    }
+    
+    void add(int key) {
+        arr[key] = true;
+    }
+    
+    void remove(int key) {
+        arr[key] = false;
+    }
+    
+    bool contains(int key) {
+        return arr[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
