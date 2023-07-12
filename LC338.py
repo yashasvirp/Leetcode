435d797ed2c7621fc0d0f42a344ff1292dcc81b0
Yashasvi R P <yashasvirp@gmail.com>
2023-07-12 21:48:21 +0530

    Counting Bits

---
+++ LC338.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        l = []
+        for i in range(n+1):
+            l.append(bin(i).count('1'))
+        
+        return l
