e26ff9ba390b10dd98b10aab2d8b3d08f9c4f572
Yashasvi R P <yashasvirp@gmail.com>
2023-08-06 11:17:18 +0530

    Insert Delete GetRandom O(1)

---
+++ LC380.cpp
@@ -0,0 +1,44 @@
+class RandomizedSet {
+public:
+    unordered_map <int, int> m;
+    vector<int> v;
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(m.find(val) != m.end())
+            return false;
+        
+        v.push_back(val);
+        m[val] = v.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(m.find(val) == m.end()){
+            return false;
+        }
+
+        int temp = v.back();
+        m[temp] = m[val];
+        v[m[val]] = temp;
+        v.pop_back();
+        m.erase(val);
+
+        return true;
+
+    }
+    
+    int getRandom() {
+        return v[rand() % v.size()] ;
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
