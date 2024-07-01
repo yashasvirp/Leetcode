5e79afc9aa21680b38e69cf6e6759b7c93e62497
Yashasvi R P <yashasvirp@gmail.com>
2024-07-01 22:08:08 +0530

    Three Consecutive Odds

---
+++ LC1550.py
@@ -0,0 +1,9 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(1, len(arr)-1):
+            if not arr[i]%2:
+                i += 1
+            elif arr[i-1]%2 and arr[i+1]%2:
+                return True
+        
+        return False
